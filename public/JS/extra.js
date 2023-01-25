16cecd873c2c82969667a13c7a381d962d2fd513
DelayedBrick <109932192+DelayedBrick@users.noreply.github.com>
2023-01-24 19:03:10 -0600

    Add files via upload

--- public/JS/extra.js
+++ public/JS/extra.js
@@ -1,11 +1,21 @@
-function asd(){
-    const logIns={
-        Username:req.body.UName,
-        password:req.body.PWName,
-        Email:req.body.Email
-    }
-    if(logIns.Email === "" || logIns.Username === "" || logIns.password === ""){
-        alert("a;sldkfjas;dflk");
-    }
-    alert("you have succesfuly been signed in 😊");
-};
+
+function remove1(spinner){
+    document.querySelector(".spinner-" + spinner).style.visibility = "hidden";
+    document.querySelector(".spinner-" + spinner).style.position = "absolute";
+}
+function remove2(spinner){
+    document.querySelector(".spinner-" + spinner).style.visibility = "hidden";
+    document.querySelector(".spinner-" + spinner).style.position = "absolute";
+}
+function remove3(spinner){
+    document.querySelector(".spinner-" + spinner).style.visibility = "hidden";
+    document.querySelector(".spinner-" + spinner).style.position = "absolute";
+}
+function remove4(spinner){
+    document.querySelector(".spinner-" + spinner).style.visibility = "hidden";
+    document.querySelector(".spinner-" + spinner).style.position = "absolute";
+}
+function remove5(spinner){
+    document.querySelector(".spinner-" + spinner).style.visibility = "hidden";
+    document.querySelector(".spinner-" + spinner).style.position = "absolute";
+}
